8b0740e4fcb6b3229a8637631901ef8a3a2f2511
MigueBarbas <B00157812@mytudublin.ie>
2023-12-16 19:59:46 +0000

    test is the name of the files from unity
    added

---
+++ test/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject plane;
+    private Vector3 offset = new Vector3(30, 0, 10);
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.position = plane.transform.position + offset;
+    }
+}
---
+++ test/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float rotationSpeed;
+    public float verticalInput;
+
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+        transform.Rotate(Vector3.right * rotationSpeed * verticalInput * Time.deltaTime);
+    }
+}
